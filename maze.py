9565ef4c8484b4d5b711031ed052360b797ec86c
Tertle950 <tertle950@gmail.com>
2022-05-26 14:25:49 +0000

    main

--- maze.py
+++ maze.py
@@ -7,7 +7,14 @@ import libxmplite
 
 print("Terminal-based slippery maze game on Python - Sample create task")
 
+# import only system from os
+from os import system, name
+  
+# import sleep to show output for some time period
+from time import sleep
+
 def printMaze(maze):
+    clear()
     returner = [-1, -1]
     for dex,i in enumerate(maze):
         line = ""
@@ -19,13 +26,14 @@ def printMaze(maze):
     return returner
 
 def dontPrintMaze(maze):
+    clear()
     returner = [-1, -1]
     for dex,i in enumerate(maze):
         line = ""
         for ind,j in enumerate(i):
-            if j == "☺":
-                returner = [dex, ind]
-                line += "☺"
+            if j == "☺" or j == "░":
+                if j == "☺": returner = [dex, ind]
+                line += j
             else:
                 line += " "
         print(line)
@@ -36,15 +44,15 @@ def dontPrintMaze(maze):
 # The maze itself is a constant.
 mazeStrings = [
     "████████████",
-    "█☺      █  █",
-    "█ ██ ██ █ ██",
-    "█  █ █     █",
-    "██ █   ██ ██",
-    "█    ██   ██",
-    "█ ██ █     █",
-    "█      █   █",
-    "█ █ █████ ██",
-    "█ █       ░█",
+    "█☺ █       █",
+    "█    █     █",
+    "█   ███    █",
+    "███    █   █",
+    "█       █  █",
+    "██  █     ██",
+    "█  █ █   █  ",
+    "█     █ █ ██",
+    "█  █      ░█",
     "████████████"] # It's a lot easier to write it this way.
 
 # ...but here's what I'm REALLY gonna use!
@@ -56,7 +64,14 @@ for i in mazeStrings:
     mazeChars.append(mCpart)
 
 smileyCoord = printMaze(mazeChars)
-goalCoord = [9, 10]
+
+goalCoord = None
+for idex,i in enumerate(mazeChars):
+    if(goalCoord != None): break
+    for jdex,j in enumerate(i):
+        if(j == "░"):
+            goalCoord = []
+
 
 while True:
     # Ask for input from the player. UDLR accepted.
@@ -80,24 +95,13 @@ while True:
 
     #print("ch is: {}".format(ch))
 
-    # Move the character as much as possible.
-    new = [smileyCoord[0],smileyCoord[1]]
-    if (ch != [0, 0]): 
-        while (mazeChars[new[0]][new[1]] != "█"): # This is a mess.
-            new[0] += ch[0]
-            new[1] += ch[1]
-            #printMaze(mazeChars)
-            #print(new)
-            #print(mazeChars[new[0]][new[1]])
-    else:
+    if(ch[0] + ch[1] == 0):
         print("☺  { Please type a direction for me to go. )")
         continue
-
-    # Unmove the character before he crashes into a wall. This is a mess.
-    new[0] -= ch[0]
-    new[1] -= ch[1]
-    #print(new)
-
+    new = [smileyCoord[0] + ch[0], smileyCoord[1] + ch[1]]
+    if (mazeChars[new[0]][new[1]] == "█"): # This is a mess.
+        print("☺  { Please type a direction for me to go. )")
+        continue
 
     #print("smileyCoord was: {}".format(smileyCoord))
     mazeChars[smileyCoord[0]][smileyCoord[1]] = " "
